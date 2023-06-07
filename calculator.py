1f8a822a515afa0a7a29fda80df0fc8ca8285c99
Gkibria <gkibria121@gmail.com>
2023-06-07 20:07:46 +0600

    initail commit

---
+++ calculator.py
@@ -0,0 +1,13 @@
+from calculator.calculator import Calculator
+
+class Variable:
+
+
+    print("variable")
+
+
+
+
+calculator = Calculator()
+calculator.evaluate('1+2*2')
+calculator.recorder.get_record('1+2*2')
