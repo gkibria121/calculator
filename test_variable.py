e6a2ffd75495721df26578caa37801913ab1c96f
Ahmed Kibria <gkibria121@gmail.com>
2023-06-08 19:02:23 +0600

    update

---
+++ test_variable.py
@@ -0,0 +1,18 @@
+import unittest
+
+from variable import VariableHandler:
+class ValidationTestCase(unittest.TestCase):
+
+
+    def setUp(self):
+
+
+        self.variable= VariableHandler()
+
+    def test_validation(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
